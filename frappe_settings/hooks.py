810a56ae4ac8c6356060d0f638d6fa48f734dfa2
root <root@ip-172-26-0-14.ap-southeast-1.compute.internal>
2021-04-13 03:12:03 +0000

    aa

--- frappe_settings/hooks.py
+++ frappe_settings/hooks.py
@@ -22,7 +22,7 @@ app_include_css = "/assets/frappe_settings/css/frappe_settings.css"
 app_include_js = "/assets/frappe_settings/js/frappe_settings.js"
 
 # include js, css files in header of web template
-# web_include_css = "/assets/frappe_settings/css/frappe_settings.css"
+web_include_css = "/assets/frappe_settings/css/frappe_settings.css"
 # web_include_js = "/assets/frappe_settings/js/frappe_settings.js"
 
 # include js in page
